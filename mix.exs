2e8eaea10f5e10f8200a0e7c6c0490b75b14e560
dima <usdim757@gmail.com>
2024-10-04 17:43:04 +0900

    add: basic mqtt

--- mix.exs
+++ mix.exs
@@ -14,6 +14,7 @@ defmodule MyMqtt.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
+      applications: [:amqp],
       extra_applications: [:logger],
       mod: {MyMqtt.Application, []}
     ]
@@ -22,6 +23,8 @@ defmodule MyMqtt.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      {:tortoise, "~> 0.10.0"},
+      {:amqp, "~> 3.3"},
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
